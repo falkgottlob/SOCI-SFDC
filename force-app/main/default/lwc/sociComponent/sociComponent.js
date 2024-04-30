6caf9b1dc6f65c8c91bd7c6bb478c81fc676554c
Falk Gottlob <fg1302@gmail.com>
2024-04-30 11:47:22 -0700

    Init

--- force-app/main/default/lwc/sociComponent/sociComponent.js
+++ force-app/main/default/lwc/sociComponent/sociComponent.js
@@ -1,4 +1,6 @@
-import { api, LightningElement } from 'lwc';
+import { api, wire, LightningElement } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import SOCI_REVIEW_ID from '@salesforce/schema/Case.SociReviewID__c';
 
 export default class IFrame extends LightningElement {
   @api height = '500px';
@@ -9,10 +11,29 @@ export default class IFrame extends LightningElement {
   @api productlink = '';
   @api apikey = '';
   @api width = '100%';
+
+  @api recordId; // This should be provided by the parent component or record page
+
+    @wire(getRecord, { recordId: '$recordId', fields: [CUSTOM_FIELD] })
+    caseRecord;
+
+    get customField() {
+        return this.caseRecord.data ? getFieldValue(this.caseRecord.data, CUSTOM_FIELD) : '';
+    }
  
   get fullUrl() {
     return `${this.url}/${this.accountid}/${this.productlink}?api_key=${this.apikey}#header_disabled`;
   }
+  get customFieldValue() {
+    if (this.caseRecord.error) {
+        // Handle error appropriately
+        console.error('Error fetching case:', this.caseRecord.error);
+        return undefined;
+    } else if (this.caseRecord.data) {
+        return getFieldValue(this.caseRecord.data, CUSTOM_FIELD);
+    }
+    return undefined;
+}
 
   renderedCallback() {
     if (this.sandbox) {
